3741c7e07cb2b9de1a301b5be1478c306d5a8733
Jiří Hušek <husekjiri@sssvt.cz>
2022-04-19 21:23:11 +0200

    problem s formem v loginu

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SessionsService} from '../../../services/sessions.service';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
 })
-export class LoginComponent {
+export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private service: SessionsService) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: [ '', Validators.required ],
+      password: [ '', Validators.required ]
+    });
+  }
+
+  public submit(): void {
+    this.service.login(this.form.value).subscribe(result => {
+      if (result) {
+        this.router.navigate([ '/' ]);
+      }
+    });
+  }
 
 }
