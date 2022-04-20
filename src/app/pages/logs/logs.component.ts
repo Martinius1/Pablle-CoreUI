848980d07f56e0604ed97f34a8c669842620bfcb
Perbex <bohunekdavid9@gmail.com>
2022-04-20 21:37:47 +0200

    snad oprava

--- src/app/pages/logs/logs.component.ts
+++ src/app/pages/logs/logs.component.ts
@@ -11,14 +11,24 @@ import {NgbCalendar, NgbDateStruct, NgbModule} from '@ng-bootstrap/ng-bootstrap'
 export class LogsComponent implements OnInit {
   model!: NgbDateStruct;
   date!: { year: number; month: number; };
+  showMe: boolean = true;
+
   constructor(private calendar: NgbCalendar) { }
   selectToday() {
     this.model = this.calendar.getToday();
   }
   ngOnInit(): void {
   }
+  toogleTag()
+  {
+    this.showMe=!this.showMe;
+  }
+
 
 }
+
+
+
 imports: [
   NgbModule
 ]
